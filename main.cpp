2a128981c96270630c30e8d1f41e6489be420e17
Stefano Cherubin <stefano.cherubin@polimi.it>
2017-04-29 15:33:53 +0200

    [main] new kernel + new workload generation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "kernel.hpp"
 #include "TimeMonitor.hpp"
 #include "utils.hpp"
+#include "Workload.hpp"
 
 #include "versioningCompiler/Utils.hpp"
 
 #include <sched.h>
+#include <map>
 
 #ifndef _PATH_TO_KERNEL
 #define PATH_TO_KERNEL "../"
@@ -13,12 +15,13 @@
 #endif
 
 typedef void (*kernel_t)(std::vector<int32_t> &array, const int32_t, const int32_t);
+std::map<size_t, std::shared_ptr<vc::Version> > memo;
 
 uint32_t seed = 666;
 const size_t MAX_ITERATIONS = 100;
+const float similarity_ratio_recompilation_threshold = 2.0f;
 
-void run_test(size_t data_size, int32_t min, int32_t max, size_t iterations);
-void run_test_ll(size_t data_size, int32_t min, int32_t max, size_t iterations);
+void run_test(size_t data_size, size_t iterations);
 
 int main(int argc, char const *argv[]) {
 
@@ -31,39 +34,21 @@ int main(int argc, char const *argv[]) {
                                         10*1000*1000,
                                        100*1000*1000,
                                       1000*1000*1000,
+                                   10*1000*1000*1000,
+                                  100*1000*1000*1000,
                                         };
-  const std::vector<int32_t> min = {   0, -1024,   0,    0};
-  const std::vector<int32_t> max = {1024,  1024, 541, 7919};
 
   vc::vc_utils_init();
 
   for (const size_t s : data_size) {
-    for (size_t i = 0; i < min.size(); i++) {
-      // run_test_ll(s, min[i], max[i], MAX_ITERATIONS);
-      run_test(s, min[i], max[i], MAX_ITERATIONS);
-    }
+    run_test(s, MAX_ITERATIONS);
   }
 
   return 0;
 }
 
-void run_test(size_t data_size, int32_t min, int32_t max, size_t iterations) {
-
-  TimeMonitor time_monitor1;
-  TimeMonitor time_monitor2;
-  static TimeMonitor time_monitor_ovh;
-
-  // running reference version - statically linked
-  std::vector<int32_t> v(data_size);
-  for (size_t i = 0; i < iterations; i++) {
-    initialize(v, min, max, seed++);
-    time_monitor1.start();
-    sort(v, min, max);
-    time_monitor1.stop();
-  }
-
-  // preparing for dynamic compilation
-  time_monitor_ovh.start();
+// dynamically create a new version
+std::shared_ptr<vc::Version> dynamicCompile(int32_t min, int32_t max) {
   const std::string kernel_dir = PATH_TO_KERNEL;
   const std::string kernel_file = kernel_dir + "kernel.cpp";
   const std::string functionName = "vc_sort";
@@ -74,79 +59,67 @@ void run_test(size_t data_size, int32_t min, int32_t max, size_t iterations) {
     vc::Option("min range", "-D_MIN_VALUE_RANGE=", std::to_string(min)),
     vc::Option("max range", "-D_MAX_VALUE_RANGE=", std::to_string(max)),
   };
-  auto version = vc::createVersion(kernel_file, functionName, opt_list);
+  std::shared_ptr<vc::Version> version = vc::createVersion(kernel_file, functionName, opt_list);
   // compiling...
   kernel_t f = (kernel_t) vc::compileAndGetSymbol(version);
-  if (! f) {
-    std::cerr << "error compiling kernel version" << std::endl;
-    return;
+  if (f) {
+    return version;
   }
-  time_monitor_ovh.stop();
-
-  // running dynamically compiled version
-  for (size_t i = 0; i < iterations; i++) {
-    initialize(v, min, max, seed++);
-    time_monitor2.start();
-    f(v);
-    time_monitor2.stop();
-  }
-
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken old " << time_monitor1.getAvg() << " ms" << std::endl;
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken vc  " << time_monitor2.getAvg() << " ms" << std::endl;
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken ovh " << time_monitor_ovh.getAvg() << " ms" << std::endl;
-  return;
+  return nullptr;
 }
 
-// same as run_test, but uses low-level APIs
-void run_test_ll(size_t data_size, int32_t min, int32_t max, size_t iterations) {
+// lookup in previously compiled versions
+std::shared_ptr<vc::Version> getDynamicVersion(int32_t min, int32_t max) {
+  const size_t range = max - min;
+  auto elem = memo.upper_bound(range);
+  size_t threshold = range * similarity_ratio_recompilation_threshold;
+  if (elem != memo.end() && elem->first < threshold) {
+    elem->second->compile();
+    return elem->second;
+  }
+  auto v = dynamicCompile(min, max);
+  if (v != nullptr) {
+    memo[range] = v;
+  }
+  return v;
+}
 
-  TimeMonitor time_monitor1;
-  TimeMonitor time_monitor2;
-  static TimeMonitor time_monitor_ovh;
+void run_test(size_t data_size, size_t iterations) {
 
-  std::vector<int32_t> v(data_size);
+  TimeMonitor time_monitor_ref;
+  TimeMonitor time_monitor_dyn;
+  TimeMonitor time_monitor_ovh;
 
-  // running reference version - statically linked
+  // running reference version - statically linked to main program
   for (size_t i = 0; i < iterations; i++) {
-    initialize(v, min, max, seed++);
-    time_monitor1.start();
-    sort(v, min, max);
-    time_monitor1.stop();
+    auto wl = WorkloadProducer<int32_t>::get_WL_with_size(data_size, seed);
+    const auto meta = wl.getMetadata();
+    time_monitor_ref.start();
+    sort(wl.data, meta.minVal, meta.maxVal);
+    time_monitor_ref.stop();
   }
 
-  // preparing for dynamic compilation
-  time_monitor_ovh.start();
-  vc::Version::Builder builder;
-  const std::string kernel_dir = PATH_TO_KERNEL;
-  builder._fileName_src = kernel_dir + "kernel.cpp";
-  builder._compiler = std::make_shared<vc::SystemCompiler>();
-  builder._functionName = "vc_sort";
-  builder._optionList = {
-    vc::Option("opt lv", "-O", "3"),
-    vc::Option("std", "-std=", "c++11"),
-    vc::Option("include dir", "-I", kernel_dir),
-    vc::Option("min range", "-D_MIN_VALUE_RANGE=", std::to_string(min)),
-    vc::Option("max range", "-D_MAX_VALUE_RANGE=", std::to_string(max)),
-  };
-  auto version = builder.build();
-  // compiling...
-  if (! version->compile()) {
-    std::cerr << "error compiling kernel version" << std::endl;
-    return;
-  }
-  kernel_t f = (kernel_t) version->getSymbol();
-  time_monitor_ovh.stop();
-
-  // running dynamically compiled version
+  // running dynamic version - dynamically compiled
   for (size_t i = 0; i < iterations; i++) {
-    initialize(v, min, max, seed++);
-    time_monitor2.start();
-    f(v);
-    time_monitor2.stop();
+    auto wl = WorkloadProducer<int32_t>::get_WL_with_size(data_size, seed);
+    const auto meta = wl.getMetadata();
+    time_monitor_ovh.start();
+    auto v = getDynamicVersion(meta.minVal, meta.maxVal);
+    kernel_t my_sort = (kernel_t) v->getSymbol();
+    time_monitor_ovh.stop();
+    if (! my_sort) {
+      std::cerr << "Error while processing item " << i << std::endl;
+      return;
+    }
+    time_monitor_dyn.start();
+    my_sort(wl.data, meta.minVal, meta.maxVal);
+    v->fold();
+    time_monitor_dyn.stop();
   }
 
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken old " << time_monitor1.getAvg() << " ms" << std::endl;
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken vc  " << time_monitor2.getAvg() << " ms" << std::endl;
-  std::cout << data_size << "\t[ " << min << ", " << max << " )\t Avg Time taken ovh " << time_monitor_ovh.getAvg() << " ms" << std::endl;
+  std::cout << data_size << "\t Avg Time taken ref " << time_monitor_ref.getAvg() << " ms" << std::endl;
+  std::cout << data_size << "\t Avg Time taken dyn " << time_monitor_dyn.getAvg() << " ms" << std::endl;
+  std::cout << data_size << "\t Max Time taken ovh " << time_monitor_ovh.getMax() << " ms" << std::endl;
+  std::cout << data_size << "\t Avg Time taken ovh " << time_monitor_ovh.getAvg() << " ms" << std::endl;
   return;
 }
